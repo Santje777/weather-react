17ec96302622edbe41e90814ac2ba618f0b0fe4e
Santje777 <s.v.oosterom@gmail.com>
2024-06-14 23:51:33 +0200

    Added forecast 1 day ahead

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import WeatherIcon from "./WeatherIcon.js";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
 const [loaded, setLoaded] = useState(false);
@@ -19,9 +19,7 @@ const [forecast, setForecast] = useState(null);
           <div className="card-body">
               <div className="row">
               <div className="col">
-            <h3>Thursday</h3>
-            <WeatherIcon code="09d" size={70} color={"#0d6efd"}/>
-            <span className="card-two-screen"><b>19°C</b>  10°C</span>
+                <WeatherForecastDay data={forecast[0]} />
              </div>
             </div>
           </div>
@@ -32,7 +30,7 @@ const [forecast, setForecast] = useState(null);
     let apiKey= "3734of2cfc5035aca32f96t5a9b478fb";
     let longitude= props.coordinates.lon;
     let latitude= props.coordinates.lat;
-    let apiUrl= `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    let apiUrl= `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
     
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,26 @@
+export default function WeatherForecastDay(props) {
+    function maxTemperature() {
+        let temperature = Math.round(props.data.temperature.maximum);
+        return `${temperature} °C`
+    }
+
+    function minTemperature() {
+        let temperature = Math.round(props.data.temperature.minimum);
+        return `${temperature} °C`
+    }
+
+    function day() {
+        let date = new Date(props.data.time * 1000);
+        let day = date.getDay();
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+        return days[day];
+    }
+
+    return (
+       <div className="WeatherForecast-day">
+    <h3>{day()}</h3>
+    <img src={props.data.condition.icon_url} alt="weather icon"/>
+    <span className="card-two-screen"><b>{maxTemperature()}</b> {minTemperature()}</span>
+</div>
+)}
